c6edd2357a08d45fdb86c6297ae776e1dc255ce1
kunjalkunjal <kunjalmistry109@gmail.com>
2022-04-01 01:32:58 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,6 +2,6 @@
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& f_stats) {
     //Implement statistics here
-    Stats m_stats(f_stats);
-    return m_stats;
+    //Stats m_stats(f_stats);
+    //return m_stats;
 }
