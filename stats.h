00acc804c35fa1d6e823c51b2fbb79348069318b
kunjalkunjal <kunjalmistry109@gmail.com>
2022-04-01 00:47:05 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,32 @@
 #include <vector>
 
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<float>& );
 }
+
+class EmailAlert
+{
+public:
+int8_t emailSent;
+};
+
+class LEDAlert
+{
+public:
+int8_t ledGlows;
+};
+
+template<typename T1>
+class IAlerter
+{
+public:
+T1* IAlert_p;
+}
+
+class StatsAlerter
+{
+public:
+void checkAndAlert(const std::vector<float>&)
+{}
+
+};
